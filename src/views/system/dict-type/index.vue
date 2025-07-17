bb08b7549d8c484edfcfbfa60d3be298caf3a9a6
Mango Crisp <15014633363@163.com>
2025-07-17 15:51:23 +0800

    fix: style 检测？

--- src/views/system/dict-type/index.vue
+++ src/views/system/dict-type/index.vue
@@ -7,3 +7,19 @@ defineOptions({
 <template>
   <div class="main">字典管理</div>
 </template>
+
+<style lang="scss" scoped>
+:deep(.el-dropdown-menu__item i) {
+  margin: 0;
+}
+
+.main-content {
+  margin: 24px 24px 0 !important;
+}
+
+.search-form {
+  :deep(.el-form-item) {
+    margin-bottom: 12px;
+  }
+}
+</style>
